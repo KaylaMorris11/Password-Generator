69c83b37fe8a30f6147ab758188233a99fc3e98a
Kayla Morris <moreiskay14@gmail.com>
2021-05-22 22:49:19 -0700

    Added the necessary code to get all the prompts to pop up

--- script.js
+++ script.js
@@ -6,25 +6,31 @@ var lowerCaseLtrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m",
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-// var all = upperCaseLtrs.concat(lowerCaseLtrs, numbers, symbols);
+var all = upperCaseLtrs.concat(lowerCaseLtrs, numbers, symbols);
 
 function writePassword() {
-  preventDefault
   var password = "";
   var wantUpperCase = true;
   var wantLowerCase = true;
   var wantNumbers = true;
   var wantSymbols = true;
-
+  
   var length = prompt("How many characters do you want in your password?");
-  if (length < 8 || length > 128) {
-    alert("Password must be 8 to 128 characters long.")
-    prompt("How many characters do you want in  your password?")
-  }
-  // wantUpperCase = confirm("Do you want lower case letters in your password?");
 
+// for (var i = 0); i < all.length; i++)
+
+  if (length < 8 || length > 128) {
+    alert("Password must be 8 to 128 characters long.")}
+wantUpperCase = confirm("Do you want lower case letters in your password?")
+wantLowerCase = confirm("Do you want numbers in your password?")
+wantNumbers = confirm("Do you want numbers in your password?")
+wantSymbols = confirm("Do you want symbols in your password?")
+
+  wantLower
   if (wantUppercase === true) {
     confirm("Do you want lower case letters in your password?")
+  }
+  else if (!wantUpperCase) {
   }
   if (wantLowerCase === true) {
     confirm("Do you want numbers in your password?")
@@ -35,10 +41,10 @@ function writePassword() {
   // else (wantSymbols === true){
   //   //  writePassword()
   // }
-
+return password;
 }
 
-// var password = generatePassword();
+var password = generatePassword();
 
 var passwordText = document.querySelector("#password")
 
